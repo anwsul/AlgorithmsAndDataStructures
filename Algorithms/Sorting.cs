5f75f4422b9aff1fa570e8cabb771ea7ade20bb7
anwsul <ansu.khiyar@gmail.com>
2025-04-12 12:19:57 +0300

    Selection sort

--- Algorithms/Sorting.cs
+++ Algorithms/Sorting.cs
@@ -11,4 +11,16 @@ class Sorting {
         }
         return array;
     }
+
+    public static void SelectionSort(int[] array) {
+        for (int i = 0; i < array.Length - 1; i++) {
+            int smallestIndex = i;
+            for (int j = i + 1; j < array.Length; j++) {
+                if (array[j] < array[smallestIndex]) {
+                    smallestIndex = j;
+                }
+            }
+            (array[i], array[smallestIndex]) = (array[smallestIndex], array[i]);
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
-﻿class Program {
+﻿using System.Collections;
+
+class Program {
     public static void Main() {
-        List<int> sortedItems = Sorting.InsertionSort([3, 1, 2, 5, 4]);
-        foreach (int item in sortedItems) {
-            Console.WriteLine(item);
-        }
     }
 }
